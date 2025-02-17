5f4a760821c008d8654781619dbf75a9261a7bac
Andy <andy.grava@gmail.com>
2025-02-17 12:16:23 +0100

    Initial Commit

---
+++ IndyCrm/IndyCrmApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndyCrmApp.swift
+//  IndyCrm
+//
+//  Created by Grava Andy on 17/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct IndyCrmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IndyCrmTests/IndyCrmTests.swift
@@ -0,0 +1,17 @@
+//
+//  IndyCrmTests.swift
+//  IndyCrmTests
+//
+//  Created by Grava Andy on 17/02/2025.
+//
+
+import Testing
+@testable import IndyCrm
+
+struct IndyCrmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
